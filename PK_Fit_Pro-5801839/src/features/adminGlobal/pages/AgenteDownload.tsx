a175c2bb1214c6f2a9401bf70a71594b31a38346
Paulo <paulohenriquegouveia08@gmail.com>
2026-04-26 18:59:00 -0300

    agente

--- PK_Fit_Pro-5801839/src/features/adminGlobal/pages/AgenteDownload.tsx
+++ PK_Fit_Pro-5801839/src/features/adminGlobal/pages/AgenteDownload.tsx
@@ -142,7 +142,7 @@ export default function AgenteDownload() {
                         <a
                             href={AGENT_DOWNLOAD_URL}
                             className="agent-download-btn"
-                            download
+                            download="PKFitAgent-Setup.exe"
                         >
                             <svg viewBox="0 0 24 24" fill="currentColor">
                                 <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
